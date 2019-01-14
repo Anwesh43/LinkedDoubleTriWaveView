c5fc8db5e3646c96327677e72d1d40d395d34088
Anwesh43 <anweshthecool0@gmail.com>
2019-01-14 10:23:29 +0530

    adding DoubleTriWave class

--- doubletriwaveview/src/main/java/com/anwesh/uiprojects/doubletriwaveview/DoubleTriWaveView.kt
+++ doubletriwaveview/src/main/java/com/anwesh/uiprojects/doubletriwaveview/DoubleTriWaveView.kt
@@ -167,4 +167,29 @@ class DoubleTriWaveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class DoubleTriWave(var i : Int) {
+
+        private val root : DTWNode = DTWNode(0)
+        private var curr : DTWNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
 }
