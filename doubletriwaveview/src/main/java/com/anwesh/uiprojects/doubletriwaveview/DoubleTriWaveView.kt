3c12a93386ac3e0edd5839bd6fc7938166d319c3
Anwesh43 <anweshthecool0@gmail.com>
2019-01-14 10:20:29 +0530

    adding DTWNode class

--- doubletriwaveview/src/main/java/com/anwesh/uiprojects/doubletriwaveview/DoubleTriWaveView.kt
+++ doubletriwaveview/src/main/java/com/anwesh/uiprojects/doubletriwaveview/DoubleTriWaveView.kt
@@ -124,4 +124,47 @@ class DoubleTriWaveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DTWNode(var i : Int, val state : State = State()) {
+        private var next : DTWNode? = null
+        private var prev : DTWNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = DTWNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDTWNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DTWNode {
+            var curr : DTWNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
